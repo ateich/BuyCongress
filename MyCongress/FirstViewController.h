fd9b1a0f560732c1c56b0b9f13aa76387bf66156
Andrew Teich <andrewteich@me.com>
2014-12-11 16:00:12 -0800

    Initial Commit

---
+++ MyCongress/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MyCongress
+//
+//  Created by HackReactor on 12/11/14.
+//  Copyright (c) 2014 HackReactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MyCongress/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyCongress
+//
+//  Created by HackReactor on 12/11/14.
+//  Copyright (c) 2014 HackReactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
