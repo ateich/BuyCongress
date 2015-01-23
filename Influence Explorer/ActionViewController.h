802936d0e17dddfe0641edffea3622e352701ad6
Andrew Teich <andrewteich@me.com>
2015-01-23 13:32:47 -0800

    Added basic action extension.

---
+++ Influence Explorer/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  Influence Explorer
+//
+//  Created by HackReactor on 1/22/15.
+//  Copyright (c) 2015 HackReactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
---
+++ MyCongress/Tokens.m
@@ -0,0 +1,26 @@
+//
+//  Tokens.m
+//  MyCongress
+//
+//  Created by HackReactor on 1/23/15.
+//  Copyright (c) 2015 HackReactor. All rights reserved.
+//
+//  INSERT YOUR OWN API TOKENS BELOW
+
+#import "Tokens.h"
+
+@implementation Tokens
+
+-(NSString*)getSunlightToken{
+    return @"";
+}
+
+-(NSString*)getTransparencyToken{
+    return @"";
+}
+
+-(NSString*)getReadabilityToken{
+    return @"";
+}
+
+@end
